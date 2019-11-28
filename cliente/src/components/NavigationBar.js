03f0596ee50f955c93096d6efd50457bef85189e
Anthpanda <Anthony.otoniel@hotmail.com>
2019-11-27 21:56:53 -0400

    Cambiando el titulo del navbar

--- cliente/src/components/NavigationBar.js
+++ cliente/src/components/NavigationBar.js
@@ -18,7 +18,7 @@ const Styles = styled.div`
 export const NavigationBar = () => (
   <Styles>
     <Navbar expand="lg">
-      <Navbar.Brand href="/">Code Life</Navbar.Brand>
+      <Navbar.Brand href="/">Grupo E</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
